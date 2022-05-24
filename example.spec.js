28aab0bbae16aff551c59c88c21b379887c930be
Andrii Zilnyk <andrii.zilnyk@eleks.com>
2022-05-24 08:58:21 +0300

    added test runner

---
+++ example.spec.js
@@ -0,0 +1,18 @@
+function getFruit() {
+  return 'apple';
+}
+
+describe('Login Page Test', function () {
+  it('Open website', function () {
+    // Code goes here
+  });
+
+  it('Go to the Login Section', function () {
+    // Code goes here
+  });
+
+  test('User can fill login form', function () {
+    // Code goes here
+    expect(getFruit()).toBe('apple');
+  });
+});
